265d7cd2445b4e08e3f4fb5571c0f466ea1720dc
williamsO03 <125751090+williamsO03@users.noreply.github.com>
2023-02-25 09:51:24 -0600

    Add files via upload

---
+++ Semana5/Program.cs
@@ -0,0 +1,16 @@
+﻿string? numero1;
+string? numero2;
+
+Console.WriteLine("Ingresa el primer valor");
+numero1 = Console.ReadLine();
+
+Console.WriteLine("Ingresa el segundo valor");
+numero2 = Console.ReadLine();
+
+for(int i = Int32.Parse(numero1); i <= Int32.Parse(numero2); i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine("Numero par: " + i);
+    }
+}
